0099b7c85d428fe81bb1a2ef0347314c2e2512f1
monst <michaeldyer1200@gmail.com>
2022-03-14 17:43:30 -0500

    Change file conversion in BigDecimalConfigurationValue to avoid potentially lossy transformation to Double.

--- src/main/java/com/monst/pluginconfiguration/impl/BigDecimalConfigurationValue.java
+++ src/main/java/com/monst/pluginconfiguration/impl/BigDecimalConfigurationValue.java
@@ -28,9 +28,18 @@ public class BigDecimalConfigurationValue extends ConfigurationValue<BigDecimal>
         return new ArgumentParseException();
     }
 
+    /**
+     * Attempts to perform a narrowing conversion to a Java Double to store in the file. If this conversion cannot be made,
+     * e.g. because this BigDecimal value cannot fit inside a Double, the string representation is returned.
+     * @param bd the value
+     * @return a Double or String representing this value
+     */
     @Override
     protected Object convertToFileData(BigDecimal bd) {
-        return bd.doubleValue();
+        Double d = bd.doubleValue();
+        if (d.isInfinite() || d.isNaN())
+            return bd.toString();
+        return d;
     }
 
 }
